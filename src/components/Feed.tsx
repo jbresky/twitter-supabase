5dc119f532c8762ed92815d749d0d985d16534e7
jbresky <joaquin_bresky@hotmail.com>
2023-10-27 23:13:01 -0300

    signup button component

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,6 +1,7 @@
 import { BsChat, BsDot, BsThreeDots } from "react-icons/bs";
 import { AiOutlineRetweet, AiOutlineHeart } from "react-icons/ai"
 import { IoStatsChart, IoShareOutline } from "react-icons/io5"
+import ComposeTweet from "./server/compose-tweet";
 
 const Feed = () => {
     return ( 
@@ -8,19 +9,7 @@ const Feed = () => {
         <h1 className="text-xl font-bold p-4 backdrop-blur bg-black/10 sticky top-0">Home</h1>
         <div className="border-t-[0.5px] border-b-[0.5px] px-4 flex items-stretch py-4 space-x-2 border-gray-600 relative">
           <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
-          <div className="flex flex-col w-full h-full">
-            <input
-              className="bg-transparent outline-none border-b-[0.5px] border-dgray-600 p-4 text-xl placeholder:text-xl placeholder:text-gray-600 border-none w-full h-full"
-              placeholder="What's happening?" />
-            <div className="w-full justify-between items-center flex">
-              <div></div>
-              <div className="w-full max-w-[100px]">
-                <button className='rounded-full bg-primary px-4 py-2 w-full text-lg font-bold text-center hover:bg-opacity-90 transition duration-200'>
-                  Tweet
-                </button>
-              </div>
-            </div>
-          </div>
+          <ComposeTweet/>
         </div>
         <div className="flex flex-col">
           {
