3f2ef2295ac422e8fc4efdfbbb43d52db7408cf0
jbresky <joaquin_bresky@hotmail.com>
2023-11-03 16:56:48 -0300

    parcial: modifying box

--- src/components/client/tweet.tsx
+++ src/components/client/tweet.tsx
@@ -32,62 +32,77 @@ const Tweet = ({
     <>
       <div
         key={tweet.tweetDetails.id}
-        className="p-4 border-b-[0.5px] border-gray-600 flex gap-4"
+        className="p-2 sm:p-4 border-b-[0.5px] border-gray-600 flex flex-col gap-4"
       >
-        <div>
-          <div className="w-10 h-10 bg-slate-200 rounded-full" />
-        </div>
-        <div className="flex flex-col space-y-2 w-[75%]">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center space-x-1 w-full">
+        {/* upper box */}
+        <div className="flex justify-between">
+          <div className="flex gap-2">
+            <div className="w-10 h-10 bg-slate-200 rounded-full" />
+            {/* <div className="flex flex-col space-y-2 w-[75%]"> */}
+            {/* <div className="flex items-start justify-between"> */}
+
+            <div className="flex max-sm:flex-col sm:gap-1 items-start text-[15px]">
 
               <div className="font-bold hover:underline cursor-pointer">{tweet.userProfile.fullName ?? ""}</div>
-              <div className="text-gray-500">@{tweet.userProfile.username}</div>
-              <div className="text-gray-500">
-                <BsDot />
-              </div>
-              <div className="text-gray-500">
-                {dayjs(tweet.tweetDetails.createdAt).fromNow()}
-              </div>
-            </div>
-            <div className="hover:text-blue-500 cursor-pointer">
-              <BsThreeDots />
-            </div>
-          </div>
-          <div className="text-white text-base">
-            {tweet.tweetDetails.text}
-          </div>
-          {/* <div className="bg-slate-400 aspect-square w-full h-96 rounded-xl"></div> */}
-          <div className="flex items-center w-full justify-between text-sm text-gray-400 pt-1">
-            <div className="flex space-x-2 items-center hover:text-blue-500 cursor-pointer">
-              <ReplyDialog tweet={tweet} repliesCount={repliesCount} />
-            </div>
-            <div className="flex space-x-2 items-center hover:text-green-500 cursor-pointer">
-              <AiOutlineRetweet />
-              <div>
-                {Math.floor(Math.random() * 1000) + 1}
-              </div>
-            </div>
-            <div className="flex space-x-2 items-center hover:text-red-500 cursor-pointer">
-              <LikeButton
-                tweetId={tweet.tweetDetails.id!}
-                likesCount={likesCount}
-                isUserHasLiked={hasLiked}
-              />
-            </div>
-            <div className="flex space-x-2 items-center hover:text-blue-500 cursor-pointer">
-              <IoStatsChart />
-              <div>
-                {Math.floor(Math.random() * 1000) + 1}
-              </div>
-            </div>
-            <div className="flex space-x-2 items-center hover:text-blue-500 cursor-pointer">
-              <IoShareOutline />
-              <div>
-                {Math.floor(Math.random() * 1000) + 1}
+              {/* user profile debe estar junto con tiempo */}
+              <div className="flex items-center">
+                <p className="text-gray-500">@{tweet.userProfile.username}</p>
+                <p className="text-gray-500">
+                  <BsDot />
+                </p>
+                <p className="text-gray-500">
+                  {dayjs(tweet.tweetDetails.createdAt).fromNow()}
+                </p>
               </div>
             </div>
           </div>
+          {/* </div> */}
+          <div className="hover:text-blue-500 cursor-pointer self-start">
+            <BsThreeDots />
+          </div>
+          {/* </div> */}
+        </div>
+
+        {/* lower box */}
+        <div className="flex justify-end">
+          <div className="w-[91%] flex flex-col">
+            <div className="text-white text-base">
+              {tweet.tweetDetails.text}
+            </div>
+            {/* <div className="bg-slate-400 aspect-square w-full h-96 rounded-xl"></div> */}
+            <div className="flex items-center w-full justify-between text-sm text-gray-400 pt-1">
+              <div className="flex items-center hover:text-blue-500 cursor-pointer">
+                <ReplyDialog tweet={tweet} repliesCount={repliesCount} />
+              </div>
+              <div className="flex space-x-2 items-center hover:text-green-500 cursor-pointer">
+                <AiOutlineRetweet />
+                <div>
+                  {/* {Math.floor(Math.random() * 1000) + 1} */}
+                </div>
+              </div>
+              <div className="flex items-center hover:text-red-500 cursor-pointer">
+                <LikeButton
+                  tweetId={tweet.tweetDetails.id!}
+                  likesCount={likesCount}
+                  isUserHasLiked={hasLiked}
+                />
+              </div>
+              <div className="flex items-center hover:text-blue-500 cursor-pointer">
+                <IoStatsChart />
+                <div>
+                  {/* {Math.floor(Math.random() * 1000) + 1} */}
+                </div>
+              </div>
+              <div className="flex items-center hover:text-blue-500 cursor-pointer">
+                <IoShareOutline />
+                <div>
+                  {/* {Math.floor(Math.random() * 1000) + 1} */}
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* lower box */}
+          {/* </div> */}
         </div>
       </div>
     </>
