556571501a9b89a3b8ee3654d8629f3378268911
jbresky <joaquin_bresky@hotmail.com>
2023-11-03 16:52:32 -0300

    getting user through server / router off

--- src/components/left-sidebar.tsx
+++ src/components/left-sidebar.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { HiOutlineHashtag } from 'react-icons/hi'
 import { BsBell, BsBookmark, BsTwitter, BsEnvelope, BsThreeDots, BsPeople } from 'react-icons/bs'
 import { BiUser } from 'react-icons/bi'
@@ -12,8 +10,9 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover"
 import { type Session } from "@supabase/auth-helpers-nextjs"
-import { createBrowserClient } from '@supabase/ssr'
-import { useRouter } from 'next/navigation'
+import { createServerClient } from '@supabase/ssr'
+// import { useRouter } from 'next/navigation'
+import { cookies } from 'next/headers'
 
 const NAVIGATION_ITEMS = [
     {
@@ -59,63 +58,72 @@ const NAVIGATION_ITEMS = [
     {
         title: 'More',
         icon: BsThreeDots
-    }
+    },
 ]
 
-const LeftSidebar = ({ session }: { session: Session | null }) => {
+const LeftSidebar = async ({ session }: { session: Session | null } ) => {
 
-    const router = useRouter()
+    // const router = useRouter()
+    const cookieStore = cookies()
 
-    const supabase = createBrowserClient(
+    const supabaseServer = createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+          cookies: {
+            get(name: string) {
+              return cookieStore.get(name)?.value
+            }
+          }
+        }
     )
 
-    const handleSignOut = async () => {
-        await supabase.auth.signOut()
-        router.refresh()
-    }
+    const {data: userData} = await supabaseServer.auth.getUser()
+
+    // const handleSignOut = async () => {
+    //     await supabase.auth.signOut()
+    //     router.refresh()
+    // }
 
     return (
-        <section className="w-[12vw] sticky top-0 sm:flex flex-col items-end px-4 pb-2 hidden">
-            <div className='flex flex-col items-stretch h-full space-y-4 my-4'>
+        <section className="2xl:w-[275px] h-screen xsm:flex flex-col items-end 2xl:items-start px-4 pb-2 overflow-y-auto hidden">
+            <div className='w-full flex flex-col items-stretch h-full space-y-4 my-4'>
                 {NAVIGATION_ITEMS.map(item => (
                     <Link
-                        className='text-xl w-fit transition duration-200 flex items-center justify-start rounded-full p-2 hover:bg-neutral-800'
-                        href={`/${item.title.toLowerCase()}`}
+                        className='text-xl w-full transition duration-200 flex items-center justify-start group'
+                        href={`/#${item.title.toLowerCase()}`}
                         key={item.title}
                     >
-                        <div className='flex items-center gap-x-5 text-center'>
+                        <div className='w-fit flex items-center text-center py-2 px-4 gap-5 rounded-full group-hover:bg-neutral-900'>
                             <div>
                                 <item.icon className='text-2xl' />
                             </div>
-                            {item.title !== 'Twitter' && <div className='hidden 3xl:flex'>{item.title}</div>}
+                            {item.title !== 'Twitter' && <div className='hidden 2xl:flex'>{item.title}</div>}
                         </div>
                     </Link>
                 ))}
-                <button className='hidden 3xl:block rounded-full mr-6 bg-primary p-4 hover:bg-opacity-90 transition duration-200'>
+                <button className='hidden 2xl:block w-full rounded-full bg-primary p-3 hover:bg-opacity-90 transition duration-200'>
                     Tweet
                 </button>
             </div>
-            {/* <Popover>
-                <PopoverTrigger className='py-2'>
-                    <button className='hidden 3xl:flex w-full items-center justify-between gap-x-2 bg-transparent p-2 mb-2 hover:bg-white/20 transition duration-200 rounded-full'>
-                        <div className='flex items-center gap-x-3'>
-                            <div className='rounded-full bg-slate-400 w-8 h-8'></div>
-                            <div className='text-left text-sx'>
-                                <div className='font-semibold text-sm'>Jbresky</div>
-                                <div className='font-semibold text-sm'>@jbreskydev</div>
+            {session && session.user.email ? (
+                <Popover>
+                    <PopoverTrigger className='py-2 w-full'>
+                        <div className='hidden 2xl:flex items-center justify-between gap-x-2 bg-transparent p-2 mb-2 hover:bg-neutral-900 transition duration-200 rounded-full'>
+                            <div className='flex items-center gap-x-3'>
+                                <div className='rounded-full bg-slate-400 w-8 h-8'></div>
+                                <div className='text-left text-sx'>
+                                    <div className='font-semibold text-sm tracking-wide'> {userData.user?.user_metadata.full_name}</div>
+                                    <div className='font-semibold text-sm text-gray-500 tracking-wide'>@{userData.user?.user_metadata.username}</div>
+                                </div>
+                            </div>
+                            <div>
+                                <BsThreeDots />
                             </div>
                         </div>
+                    </PopoverTrigger>
+                    <PopoverContent className='bg-black text-white text-sm font-bold w-full rounded-xl p-0'>
                         <div>
-                            <BsThreeDots />
-                        </div>
-                    </button>
-                    <div className="w-8 h-8 bg-slate-200 rounded-full 3xl:hidden" />
-                </PopoverTrigger>
-                <PopoverContent className='bg-black text-white text-sm font-bold w-full rounded-xl p-0'>
-                    <div>
-                        {session && session.user.email ?
                             <>
                                 <div className='hover:bg-white/10 transition duration-200 p-4 cursor-pointer'>
                                     <p>
@@ -123,24 +131,23 @@ const LeftSidebar = ({ session }: { session: Session | null }) => {
                                     </p>
 
                                 </div>
-                                <button
+                                <div
                                     className='hover:bg-white/10 flex gap-2 p-4 rounded-b-xl cursor-pointer'
-                                    onClick={handleSignOut}
+                                    // onClick={handleSignOut}
                                 >
                                     <p className=''>
                                         Log out
                                     </p>
                                     <p>
-                                        {session.user.email}
+                                        @{userData.user?.user_metadata.username}
                                     </p>
-                                </button>
+                                </div>
                             </>
-                            :
-                            null
-                        }
-                    </div>
-                </PopoverContent>
-            </Popover> */}
+
+                        </div>
+                    </PopoverContent>
+                </Popover>
+            ) : null}
         </section >
     );
 }
