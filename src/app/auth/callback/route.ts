96202288ceea9bb177a090774fef9b652e909a35
jbresky <joaquin_bresky@hotmail.com>
2023-11-03 16:53:03 -0300

    callback auth

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,23 +1,3 @@
-// import { type NextRequest, NextResponse } from "next/server";
-// import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-// import { cookies } from "next/headers";
-
-// import type { Database } from "@/lib/supabase.types";
-// export const dynamic = 'force-dynamic'
-
-// export async function GET(request: NextRequest) {
-//     const requestUrl = new URL(request.url)
-//     const code = requestUrl.searchParams.get('code')
-  
-//     if (code) {
-//       const cookieStore = cookies()
-//       const supabase = createRouteHandlerClient<Database>({ cookies: () => cookieStore })
-//       await supabase.auth.exchangeCodeForSession(code)
-//     }
-  
-//     return NextResponse.redirect(requestUrl.origin)
-//   }
-
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
@@ -54,6 +34,5 @@ export async function GET(request: Request) {
     }
   }
 
-  // return the user to an error page with instructions
   return NextResponse.redirect(new URL('/auth/auth-code-error', request.url))
 }
