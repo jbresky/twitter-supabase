a11a02c3307c38fc72ac791e92d6ab5a359b2f27
jbresky <joaquin_bresky@hotmail.com>
2023-11-07 14:51:31 -0300

    feed off

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import Feed from "@/components/feed";
 import { Toaster } from "sonner";
-import LoginFooter from "@/components/client/login-footer";
 
 const Home = async () => {
 
   return (
     <>
       <Toaster />
-      {/* @ts-expect-error Server Component */}
-      <Feed />
+      {/* <Feed /> */}
       {/* {data.session === null ? (
         <LoginFooter />
       ) : ''} */}
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,42 +1,40 @@
-import ComposeTweet from "./server/compose-tweet"
-import Tweet from "./client/tweet";
-import { getTweets } from "@/lib/supabase/queries"
-import { cookies } from "next/headers";
-import { RiTwitterXFill } from 'react-icons/ri'
-import { AiOutlineSetting } from 'react-icons/ai'
-import LoginModal from "./client/login-modal";
-import { createServerClient } from "@supabase/ssr";
+// import ComposeTweet from "./server/compose-tweet"
+// import Tweet from "./client/tweet";
+// import { getTweets } from "@/lib/supabase/queries"
+// import { cookies } from "next/headers";
+// import { RiTwitterXFill } from 'react-icons/ri'
+// import { AiOutlineSetting } from 'react-icons/ai'
+// import LoginModal from "./client/login-modal";
+// import { createServerClient } from "@supabase/ssr";
 
 const Feed = async () => {
-  const cookieStore = cookies()
+  // const cookieStore = cookies()
 
-  const supabaseServer = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        }
-      }
-    }
-  )
+  // const supabaseServer = createServerClient(
+  //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  //   {
+  //     cookies: {
+  //       get(name: string) {
+  //         return cookieStore.get(name)?.value
+  //       }
+  //     }
+  //   }
+  // )
 
-  const { data } = await supabaseServer.auth.getSession();
+  // const { data } = await supabaseServer.auth.getSession();
 
-  const { data: userData } = await supabaseServer.auth.getUser()
+  // const { data: userData } = await supabaseServer.auth.getUser()
 
-  const res = await getTweets({ currentUserID: userData.user?.id })
+  // const res = await getTweets({ currentUserID: userData.user?.id })
 
   return (
     <main className="flex w-full max-w-[600px] min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
-      {/* mobile header */}
-      <header className="h-14 w-full flex justify-between items-center md:hidden border-b-1 border-gray-500 p-4 z-10">
+      {/* <header className="h-14 w-full flex justify-between items-center md:hidden border-b-1 border-gray-500 p-4 z-10">
         <div className="w-8 h-8 bg-slate-200 rounded-full" />
         <RiTwitterXFill className="text-2xl" />
         <AiOutlineSetting className="text-xl" />
       </header>
-      {/* mobile header */}
 
       <h1 className="text-xl font-bold p-4 backdrop-blur bg-black/10 sticky top-0 z-10">Home</h1>
       <div className="border-t-[0.5px] border-b-[0.5px] px-4 flex items-stretch py-4 space-x-2 border-gray-600 relative">
@@ -75,7 +73,7 @@ const Feed = async () => {
               />
             )
           })}
-      </div>
+      </div> */}
     </main>
   );
 }
