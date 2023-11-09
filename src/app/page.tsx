d0e2dcd4c40ef4a597369bece80c092936fd7984
jbresky <joaquin_bresky@hotmail.com>
2023-11-08 21:46:43 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,28 @@ import LeftSidebar from '@/components/left-sidebar'
 import RightSection from '@/components/right-section'
 import ComposeTweet from "@/components/server/compose-tweet";
 import { redirect } from "next/navigation";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const Home = async () => {
 
-  const cookieStore = cookies()
+  // const cookieStore = cookies()
+  const supabaseServer = createServerComponentClient({ cookies })
 
-  const supabaseServer = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        }
-      }
-    }
-  )
+  // const supabaseServer = createServerClient(
+  //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  //   {
+  //     cookies: {
+  //       get(name: string) {
+  //         return cookieStore.get(name)?.value
+  //       }
+  //     }
+  //   }
+  // )
 
   const { data: userData } = await supabaseServer.auth.getUser()
   const { data: { session } } = await supabaseServer.auth.getSession()
-
+  
   const posts = await getTweets({ currentUserID: userData.user?.id })
 
   if (session === null) {
@@ -38,11 +40,11 @@ const Home = async () => {
       <Toaster />
       <LeftSidebar session={session} />
       <main className="flex w-full max-w-[600px] min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
-        <h1 className="text-xl font-bold p-4 backdrop-blur bg-black/10 sticky top-0 z-10 max-2xsm:hidden">Home</h1>
-        <div className="border-t-[0.5px] border-b-[0.5px] px-4 flex items-stretch py-4 space-x-2 border-[#71767B] relative max-2xsm:hidden">
-          <ComposeTweet userAvatar={session?.user?.user_metadata?.avatar_url} />
+        <h1 className="text-xl font-bold p-4 backdrop-blur bg-black/10 sticky top-0 z-10">Home</h1>
+        <div className="border-t-[0.5px] border-b-[0.5px] px-4 flex items-stretch py-4 space-x-2 border-gray-600 relative">
+          <ComposeTweet userAvatar={session.user?.user_metadata?.avatar_url}/>
         </div>
-        <Posts posts={posts} userAvatar={session?.user?.user_metadata?.avatar_url} />
+        <Posts posts={posts} userAvatar={session.user?.user_metadata?.avatar_url} />
       </main>
       <RightSection />
     </>
