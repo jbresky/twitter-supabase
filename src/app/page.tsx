3c53c3ae02a87798b2517e4f726069e3fe345b1f
jbresky <joaquin_bresky@hotmail.com>
2023-11-13 12:10:54 -0300

    props change: Session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,11 +38,11 @@ const Home = async () => {
       <Toaster />
       <LeftSidebar session={session} />
       <main className="flex w-full max-w-[600px] flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
-        <h1 className="text-xl font-bold p-4 backdrop-blur bg-black/10 sticky top-0 z-10 max-2xsm:hidden">Home</h1>
+        <h1 className="text-xl font-bold p-4 backdrop-blur bg-black/10 sticky top-0 z-10 max-xsm:hidden">Home</h1>
         <div className="border-t-[0.5px] px-4 flex items-stretch py-4 space-x-2 border-gray-700 relative max-xsm:hidden">
           <ComposeTweet userAvatar={session?.user?.user_metadata?.avatar_url}/>
         </div>
-        <Posts posts={posts} userAvatar={session?.user?.user_metadata?.avatar_url} />
+        <Posts posts={posts} session={session} />
       </main>
       <RightSection />
     </>
--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,40 +1,18 @@
 'use client'
 
-import { AiOutlineSetting } from "react-icons/ai";
+import { Session } from "@supabase/supabase-js";
+import MobileHeader from "./client/mobile-header";
 import Tweet from "./client/tweet";
 
 interface IPosts {
     posts: any,
-    userAvatar: string
+    session: Session
 }
 
-const Posts = ({ posts, userAvatar }: IPosts) => {
+const Posts = ({ posts, session }: IPosts) => {
     return (
         <>
-            {/* mobile header */}
-            <header className="w-full flex flex-col xsm:hidden border-b-1 border-gray-500 pt-2 z-10">
-                <div className="flex justify-between items-center px-3">
-                    <div className="flex items-center">
-                        <img className='rounded-full w-8 h-8' src={userAvatar || '/images/next.jpg'} />
-                        <h1 className="text-xl font-bold p-4 backdrop-blur bg-black/10 sticky top-0 z-10 max-2xsm:hidden">Home</h1>
-                    </div>
-                <AiOutlineSetting className="w-10 h-6" />
-                </div>
-                <div className="flex justify-around mt-4">
-                    <div className="h-10 flex flex-col gap-[14px]">
-                        <h2 className="font-semibold text-[#e7e9ea]">For you</h2>
-                        <div className='w-full rounded-xl border-b-8-primary h-[12px] bg-primary'></div>
-                    </div>
-                    <div className="">
-                        <h2 className="font-semibold text-[#71767b]">Following</h2>
-                    </div>
-                </div>
-                {/* <div className="w-full text-center py-3 border-t-[0.5px] border-gray-700">
-                    <span className="text-primary">Show 220 posts</span>
-                </div> */}
-            </header>
-            {/* mobile header */}
-
+        <MobileHeader session={session}/>
             <div className="flex flex-col">
                 {
                     posts?.map((post: any) => {
