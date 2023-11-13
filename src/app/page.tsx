73bc5f3a5636b00fd7ac4d5665e25df89c2dafbf
jbresky <joaquin_bresky@hotmail.com>
2023-11-13 16:34:42 -0300

    wrapping sidebar and logout component within a section tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import { Toaster } from "sonner";
-import { getTweets } from "@/lib/supabase/queries"
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
+import Logout from "@/components/client/logout";
 import Posts from "@/components/posts";
 import LeftSidebar from '@/components/left-sidebar'
 import RightSection from '@/components/right-section'
 import ComposeTweet from "@/components/server/compose-tweet";
+import TweetDialog from "@/components/tweet-dialog";
+import { Toaster } from "sonner";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { getTweets } from "@/lib/supabase/queries"
+import { createServerClient } from "@supabase/ssr";
 
 const Home = async () => {
 
@@ -26,7 +28,7 @@ const Home = async () => {
 
   const { data: userData } = await supabaseServer.auth.getUser()
   const { data: { session } } = await supabaseServer.auth.getSession()
-  
+
   const posts = await getTweets({ currentUserID: userData.user?.id })
 
   if (session === null) {
@@ -36,11 +38,17 @@ const Home = async () => {
   return (
     <>
       <Toaster />
-      <LeftSidebar session={session} />
+      <section className="2xl:w-[275px] sticky top-0 left-0 h-screen xsm:flex justify-between flex-col items-end 2xl:items-start px-4 pb-2 overflow-y-auto overflow-x-hidden hidden">
+        <LeftSidebar session={session} />
+        <Logout session={session}/>
+      </section>
       <main className="flex w-full max-w-[600px] flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
         <h1 className="text-xl font-bold p-4 backdrop-blur bg-black/10 sticky top-0 z-10 max-xsm:hidden">Home</h1>
         <div className="border-t-[0.5px] px-4 flex items-stretch py-4 space-x-2 border-gray-700 relative max-xsm:hidden">
-          <ComposeTweet userAvatar={session?.user?.user_metadata?.avatar_url}/>
+          <ComposeTweet userAvatar={session?.user?.user_metadata?.avatar_url} />
+        </div>
+        <div className="xsm:hidden">
+          <TweetDialog userAvatar={session?.user?.user_metadata?.avatar_url} />
         </div>
         <Posts posts={posts} session={session} />
       </main>
